9cbe2df1ea40c84bfc6472261b9f1630e787cb40
Reece Williams <reecepbcups@gmail.com>
2023-02-03 22:37:31 -0600

    add config file options

--- setup.sh
+++ setup.sh
@@ -2,9 +2,16 @@
 
 set -e
 
-# Add your moniker here
 MONIKER="YOUR_MONIKER_HERE"
+ENABLE_CORS="true"
 
+ENABLE_REST_API="true"
+REST_API_PORT="1317"
+
+PUBLIC_RPC="true"
+RPC_PORT="26657"
+
+# === Don't change anything below this line ===
 # Checks to see if you've already installed Canto
 # 
 [ -e /$HOME/Canto ] && rm -rf /$HOME/Canto
@@ -34,6 +41,20 @@ sed -i 's/seeds = ""/seeds = "ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@seeds.pol
 
 # Sets service file
 sudo cp $HOME/canto-node-primer/canto.service /etc/systemd/system/
+
+# Config Files
+if [ "$ENABLE_CORS" = "true" ]; then
+  sed -i 's/cors_allowed_origins = \[\]/cors_allowed_origins = \["\*"\]/g' /$HOME/.cantod/config/config.toml
+fi
+
+if [ "$ENABLE_REST_API" = "true" ]; then
+  sed -i 's/enable = false/enable = true/g' /$HOME/.cantod/config/app.toml
+  sed -i 's/address = "tcp:\/\/0.0.0.0:1317"/address = "tcp:\/\/0.0.0.0:'$REST_API_PORT'"/g' /$HOME/.cantod/config/app.toml
+fi
+
+if [ "$PUBLIC_RPC" = "true" ]; then
+  sed -i 's/laddr = "tcp:\/\/127.0.0.1:26657"/c\laddr = "tcp:\/\/0.0.0.0:'$RPC_PORT'"/g' $HOME_DIR/config/config.toml
+fi
 
 # Syncs node
 cd $HOME/canto-node-primer
